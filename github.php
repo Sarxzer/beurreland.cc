861d9463325e17dff4698ed5bd9d848a634fe544
Sarxzer <80457483+Sarxzer@users.noreply.github.com>
2026-04-12 21:38:48 +0200

    patch to the github page footer

--- github.php
+++ github.php
@@ -44,7 +44,7 @@
             <div id="commits"></div>
         </div>
 
-        <?php include 'footer.php'; ?>
+        <?php include 'inc/footer.php'; ?>
     </div>
 
     <?php include "inc/rsidebar.php"; ?>
