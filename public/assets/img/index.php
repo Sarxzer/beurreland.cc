2be650dfa9c10d6c950c631d1c729ca1884ab6fb
Sarxzer <80457483+Sarxzer@users.noreply.github.com>
2026-04-26 04:43:23 +0200

    refactor image display logic in index.php to use random width and remove scaling

--- public/assets/img/index.php
+++ public/assets/img/index.php
@@ -6,12 +6,12 @@ $images = array_diff($images, ['.', '..', 'index.php']);
 shuffle($images);
 foreach ($images as $image) {
     $imagePath = "/assets/img/" . $image;
-    $scale = rand(10, 100) / 100; // Random scale between 0.1 and 2.0
 
-    $width = getimagesize(__DIR__ . '/' . $image)[0] * $scale;
-    $height = getimagesize(__DIR__ . '/' . $image)[1] * $scale;
+    $randSize = rand(50, 1000);
+    
+    $width = $randSize; // Set the width to the random size
 
-    $html = "<img src=\"$imagePath\" alt=\"Image, scale: $scale\" style=\"width: {$width}px; height: {$height}px;\">";
+    $html = "<a href=\"$imagePath\"><img src=\"$imagePath\" alt=\"Image\" style=\"width: {$width}px;\" loading=\"lazy\"></a>";
 
     echo $html;
 }
